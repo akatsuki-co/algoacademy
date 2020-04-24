d67ee9af520f4b2fd086ace05b4029b8cc290d3a
Tu Vo <tuvo1106@gmail.com>
2020-04-24 14:01:04 -0700

    Add field to api

--- pages/api/python/index.js
+++ pages/api/python/index.js
@@ -1,23 +1,26 @@
-const tableOfContents = [
-  {
-    menu: "Getting Started",
-    subMenu: [
-      "Hello World",
-      "Data Types",
-      "Basic Syntax",
-      "Constants",
-      "Example",
-    ],
-  },
-  {
-    menu: "Intermediate",
-    subMenu: ["Builtins", "Data Structures", "Basic Algorithms"],
-  },
-  {
-    menu: "Advanced",
-    subMenu: ["Sorting", "Advanced Data Structures", "Big O"],
-  },
-]
+const tableOfContents = {
+  language: "python",
+  sidebar: [
+    {
+      menu: "Getting Started",
+      subMenu: [
+        "Hello World",
+        "Data Types",
+        "Basic Syntax",
+        "Constants",
+        "Example",
+      ],
+    },
+    {
+      menu: "Intermediate",
+      subMenu: ["Builtins", "Data Structures", "Basic Algorithms"],
+    },
+    {
+      menu: "Advanced",
+      subMenu: ["Sorting", "Advanced Data Structures", "Big O"],
+    },
+  ],
+}
 
 export default (req, res) => {
   res.status(200).json(tableOfContents)
