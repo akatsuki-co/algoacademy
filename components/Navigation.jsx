9c322ceb01ab9812df9a12bb9bb4fc46dd13b734
Tu Vo <tuvo1106@gmail.com>
2020-04-24 13:22:29 -0700

    Fixed class to className

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -5,37 +5,35 @@ import Form from "react-bootstrap/Form"
 import FormControl from "react-bootstrap/FormControl"
 import Button from "react-bootstrap/Button"
 
-const Navigation = () => {
-  return (
-    <div className='gradient'>
-      <Container>
-        <Navbar variant='light' expand='md'>
-          <Navbar.Brand href='/' class='d-flex align-items-center'>
-            <img src='/favicon.png' width='25' height='25'></img>
-            <span class="ml-2">Study Buddy</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar-collapse' />
-          <Navbar.Collapse id='navbar-collapse'>
-            <Nav className='mr-auto'>
-              <Nav.Link>About</Nav.Link>
-              <Nav.Link>F.A.Q.</Nav.Link>
-              <Nav.Link>Contribute</Nav.Link>
-            </Nav>
-            <Nav className='ml-auto' id='search-form'>
-              <Form inline>
-                <FormControl
-                  type='text'
-                  placeholder='Search'
-                  className='mr-sm-2'
-                />
-                <Button variant='outline-primary'>Search</Button>
-              </Form>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
-    </div>
-  )
-}
+const Navigation = () => (
+  <div className='gradient'>
+    <Container>
+      <Navbar variant='light' expand='md'>
+        <Navbar.Brand href='/' className='d-flex align-items-center'>
+          <img src='/favicon.png' width='25' height='25'></img>
+          <span className='ml-2'>Study Buddy</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar-collapse' />
+        <Navbar.Collapse id='navbar-collapse'>
+          <Nav className='mr-auto'>
+            <Nav.Link>About</Nav.Link>
+            <Nav.Link>F.A.Q.</Nav.Link>
+            <Nav.Link>Contribute</Nav.Link>
+          </Nav>
+          <Nav className='ml-auto' id='search-form'>
+            <Form inline>
+              <FormControl
+                type='text'
+                placeholder='Search'
+                className='mr-sm-2'
+              />
+              <Button variant='outline-primary'>Search</Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  </div>
+)
 
 export default Navigation
