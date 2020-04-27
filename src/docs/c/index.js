dee1f0e4e190ec57099063e1ec4863ca96898fe2
Tu Vo <tuvo1106@gmail.com>
2020-04-27 15:28:48 -0700

    Updated paths

--- src/docs/c/index.js
+++ src/docs/c/index.js
@@ -2,12 +2,18 @@ import React from "react"
 import HelloWorld from "./HelloWorld"
 import DataTypes from "./DataTypes"
 import Looping from "./Looping"
+import Arrays from "./Arrays"
+import Functions from "./Functions"
+import Structures from "./Structures"
 
 const routes = [
   { path: "/c", exact: true, component: HelloWorld },
   { path: "/c/hello_world", exact: true, component: HelloWorld },
   { path: "/c/data_types", exact: true, component: DataTypes },
   { path: "/c/looping", exact: true, component: Looping },
+  { path: "/c/arrays", exact: true, component: Arrays },
+  { path: "/c/functions", exact: true, component: Functions },
+  { path: "/c/structures", exact: true, component: Structures },
   {
     path: "/c",
     exact: false,
