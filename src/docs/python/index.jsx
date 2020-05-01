da2edd44ec9ac865d0ebe00b0612617924e3d03c
Brent Muha <800@holbertonschool.com>
2020-05-01 15:46:23 -0700

    Add Dictionaries

--- src/docs/python/index.jsx
+++ src/docs/python/index.jsx
@@ -1,12 +1,14 @@
 import HelloWorld from "./HelloWorld"
 import DataTypes from "./DataTypes"
 import Lists from "./Lists"
+import Dictionaries from "./Dictionaries"
 
 const routes = [
   { path: "/python", exact: true, component: HelloWorld },
   { path: "/python/hello_world", exact: true, component: HelloWorld },
   { path: "/python/data_types", exact: false, component: DataTypes },
   { path: "/python/lists", exact: false, component: Lists },
+  { path: "/python/dictionaries", exact: false, component: Dictionaries },
 ]
 
 export default routes
