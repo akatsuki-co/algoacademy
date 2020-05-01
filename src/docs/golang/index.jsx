1663c3e761699cbb4e7a12f7a8cd55b8bb782b37
Tu Vo <tuvo1106@gmail.com>
2020-04-30 22:07:29 -0700

    Add LinkedLists

--- src/docs/golang/index.jsx
+++ src/docs/golang/index.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import HelloWorld from "./HelloWorld"
 import DataTypes from "./DataTypes"
+import LinkedLists from "./LinkedLists"
 
 const routes = [
   { path: "/golang", exact: true, component: HelloWorld },
   { path: "/golang/hello_world", exact: true, component: HelloWorld },
   { path: "/golang/data_types", exact: true, component: DataTypes },
+  { path: "/golang/linked_lists", exact: true, component: LinkedLists },
   {
     path: "/golang",
     exact: false,
