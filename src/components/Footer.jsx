2c688d56a060c3e715df4326f00eacd739b7f579
Tu Vo <tuvo1106@gmail.com>
2020-05-03 20:15:39 -0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,11 +17,14 @@ const Footer = () => (
             <p>
               © 2020 By
               <a href='https://github.com/tuvo1106' className='text-muted'>
-                {" Tu Vo "}
+                {" Tu Vo, "}
+              </a>
+              <a href='https://github.com/rmiyazaki6499' className='text-muted'>
+                {" Ryuichi Miyazaki, "}
               </a>
               &amp;
-              <a href='https://github.com/rmiyazaki6499' className='text-muted'>
-                {" Ryuichi Miyazaki "}
+              <a href='https://github.com/bmuha1' className='text-muted'>
+                {" Brent Muha "}
               </a>
             </p>
           </div>
