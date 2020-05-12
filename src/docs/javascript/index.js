885b11498a725de265cf3b647f40e846ed16ff1a
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-05-11 20:24:28 -0700

    Convert all Javascript doc files into md files

--- src/docs/javascript/index.js
+++
@@ -1,8 +0,0 @@
-import HelloWorld from "./HelloWorld"
-
-const routes = [
-    { path: "/javascript", exact: true, component: HelloWorld },
-    { path: "/javascript/hello_world", exact: true, component: HelloWorld },
-  ]
-  
-export default routes
