3754eaa00f480054d519c063f5555f128462cd53
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-06-03 09:49:14 -0700

    Update Results

--- src/components/quiz/Results.jsx
+++ src/components/quiz/Results.jsx
@@ -1,51 +1,20 @@
-import React, { useContext } from "react"
-import QuizContext from "../../context/QuizContext"
-import { useHistory } from "react-router-dom"
-import { RESET_QUIZ } from "../../reducers/types.jsx"
+import React, { useContext } from 'react'
+import QuizContext from '../../context/QuizContext'
+import { useHistory } from 'react-router-dom'
+import { RESET_QUIZ } from '../../reducers/types.jsx'
 
-import "./styles.css"
+import './styles.css'
 
 const Results = () => {
-  const questions = [
-    {
-      id: 1,
-      question: "Which statement about Hooks is not true?",
-      answer_a:
-        "Hooks are 100% backwards-compatible and can be used side by side with classes",
-      answer_b: "Hooks are still in beta and not available yet",
-      answer_c:
-        "Hooks are completely opt-in, there's no need to rewrite existing code",
-      answer_d: "All of the above",
-      correct_answer: "b",
-    },
-    {
-      id: 2,
-      question: "Which one is not a Hook?",
-      answer_a: "useState()",
-      answer_b: "useConst()",
-      answer_c: "useReducer()",
-      answer_d: "All of the above",
-      correct_answer: "b",
-    },
-    {
-      id: 3,
-      question: "What Hook should be used for data fetching?",
-      answer_a: "useDataFetching()",
-      answer_b: "useApi()",
-      answer_c: "useEffect()",
-      answer_d: "useRequest()",
-      correct_answer: "c",
-    },
-  ]
   let history = useHistory()
   const { state, dispatch } = useContext(QuizContext)
-  const { answers } = state
+  const { answers, questions } = state
 
   const renderResultMark = (question, answer) => {
-    if (question.correct_answer === answer.answer) {
-      return <span className="correct">Correct</span>
+    if (question.correct_answer.includes(answer.answer)) {
+      return <span className='correct'>Correct</span>
     }
-    return <span className="failed">Failed</span>
+    return <span className='failed'>Failed</span>
   }
 
   const renderResultsData = () => {
@@ -63,13 +32,13 @@ const Results = () => {
 
   const restart = () => {
     dispatch({ type: RESET_QUIZ })
-    history.push("/quiz")
+    history.push('/quiz')
   }
   return (
-    <div className="quiz results gradient">
+    <div className='quiz results gradient'>
       <h2>Results</h2>
       <ul>{renderResultsData()}</ul>
-      <button className="quiz-btn btn-primary" onClick={restart}>
+      <button className='quiz-btn btn-primary' onClick={restart}>
         Go Back
       </button>
     </div>
