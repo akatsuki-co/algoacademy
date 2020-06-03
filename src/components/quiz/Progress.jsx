56fe805b30393483122587674317fbbfb89e723a
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-06-03 09:47:05 -0700

    Refactor Progress component

--- src/components/quiz/Progress.jsx
+++ src/components/quiz/Progress.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './styles.css'
 
-function Progress({ current, total }) {
+const Progress = ({ current, total }) => {
   return (
     <h2>
       Question {current} of {total}
