d01dd00e8e2ee378851851722bc97f7fb9856ad8
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-06-03 09:47:59 -0700

    Refactor Quiz component

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,106 +1,87 @@
-import React, { useReducer } from "react"
-import Progress from "./Progress"
-import Question from "./Question"
-import Answers from "./Answers"
-import Results from "./Results"
-import QuizContext from "../../context/QuizContext"
-
-import "./styles.css"
-
+import React, { useReducer } from 'react'
+import Progress from './Progress'
+import Question from './Question'
+import Answers from './Answers'
+import Results from './Results'
+import QuizContext from '../../context/QuizContext'
 import {
   SET_ANSWERS,
   SET_CURRENT_QUESTION,
+  SET_NEXT_QUESTION,
   SET_CURRENT_ANSWER,
   SET_ERROR,
   SET_SHOW_RESULTS,
-  RESET_QUIZ,
-} from "../../reducers/types.jsx"
-import quizReducer from "../../reducers/QuizReducer"
+} from '../../reducers/types.jsx'
+import quizReducer from '../../reducers/QuizReducer'
+import shuffle from '../../utils/shuffle'
+
+import data from '../../data/quiz.json'
+import './styles.css'
 
 function Quiz() {
-  const questions = [
-    {
-      id: 1,
-      question: "Which statement about Hooks is not true?",
-      answer_a:
-        "Hooks are 100% backwards-compatible and can be used side by side with classes",
-      answer_b: "Hooks are still in beta and not available yet",
-      answer_c:
-        "Hooks are completely opt-in, there's no need to rewrite existing code",
-      answer_d: "All of the above",
-      correct_answer: "b",
-    },
-    {
-      id: 2,
-      question: "Which one is not a Hook?",
-      answer_a: "useState()",
-      answer_b: "useConst()",
-      answer_c: "useReducer()",
-      answer_d: "All of the above",
-      correct_answer: "b",
-    },
-    {
-      id: 3,
-      question: "What Hook should be used for data fetching?",
-      answer_a: "useDataFetching()",
-      answer_b: "useApi()",
-      answer_c: "useEffect()",
-      answer_d: "useRequest()",
-      correct_answer: "c",
-    },
-  ]
-
   const initialState = {
-    questions,
-    currentQuestion: 0,
-    currentAnswer: "",
+    questions: data,
+    currentQuestionIndex: 0,
+    currentQuestion: null,
+    currentAnswer: '',
     answers: [],
     showResults: false,
-    error: "",
+    error: '',
   }
-
   const [state, dispatch] = useReducer(quizReducer, initialState)
-  const { currentQuestion, currentAnswer, answers, showResults, error } = state
-  const question = questions[currentQuestion]
+  const {
+    questions,
+    currentQuestionIndex,
+    currentQuestion,
+    currentAnswer,
+    answers,
+    showResults,
+    error,
+  } = state
 
   const renderError = () => {
-    if (!error) {
-      return
-    }
-    return <div className="error">{error}</div>
+    if (!error) return
+    return <div className='error'>{error}</div>
+  }
+
+  const shuffleQuiz = () => {
+    shuffle(questions)
+    dispatch({
+      type: SET_CURRENT_QUESTION,
+      currentQuestion: questions[0],
+    })
   }
 
   const next = () => {
-    const answer = { questionId: question.id, answer: currentAnswer }
-    if (!currentAnswer) {
-      dispatch({ type: SET_ERROR, error: "Please select an option" })
-      return
-    }
+    const answer = { questionId: currentQuestion.id, answer: currentAnswer }
+    if (!currentAnswer)
+      return dispatch({ type: SET_ERROR, error: 'Please select an option' })
     answers.push(answer)
     dispatch({ type: SET_ANSWERS, answers })
-    dispatch({ type: SET_CURRENT_ANSWER, currentAnswer: "" })
-    if (currentQuestion + 1 < questions.length) {
+    dispatch({ type: SET_CURRENT_ANSWER, currentAnswer: '' })
+    if (currentQuestionIndex + 1 < questions.length) {
       dispatch({
-        type: SET_CURRENT_QUESTION,
-        currentQuestion: currentQuestion + 1,
+        type: SET_NEXT_QUESTION,
+        currentQuestionIndex: currentQuestionIndex + 1,
+        currentQuestion: questions[currentQuestionIndex + 1],
       })
       return
     }
     dispatch({ type: SET_SHOW_RESULTS, showResults: true })
   }
-
   return showResults ? (
     <QuizContext.Provider value={{ state, dispatch }}>
       <Results />
     </QuizContext.Provider>
   ) : (
     <QuizContext.Provider value={{ state, dispatch }}>
-      <div className="quiz gradient py-5">
-        <Progress total={questions.length} current={currentQuestion + 1} />
+      <div className='quiz gradient py-5'>
+        {currentQuestion ? null : shuffleQuiz()}
+        <Progress total={questions.length} current={currentQuestionIndex + 1} />
         <Question />
         {renderError()}
         <Answers />
-        <button className="quiz-btn btn-primary" onClick={next}>
+        <button className='quiz-btn btn-primary' onClick={next}>
           Confirm and Continue
         </button>
       </div>
