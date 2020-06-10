6db02b76aca950fb442260818835cd645040b1f6
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-06-09 18:56:48 -0700

    Fix endpoints

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(compression())
 const router = express.Router()
 
 // Mounting Routers - API endpoints
-// app.use("/", router)
+app.use("/", router)
 app.use("/api/v1/quizzes", quizRouter)
 router.get("/test", (req, res) => {
     res.status(200).json({
--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,85 +1,75 @@
 const Quiz = require("./../models/Quiz")
 
-
+// Catches errors
 catchAsync = fn => {
-    // returns an anonymous function
     return (req, res, next) => {
         fn(req, res, next).catch(next)
     };
 }
 
-exports.deleteQuiz = () =>
-    catchAsync(async(req, res, next) => {
-        const quiz = await Quiz.findByIdAndDelete(req.params.id)
-        if (!quiz) {
-            return "No Quiz with that ID"
-        }
-        res.status(204).json({
-            status: "success",
-            data: null
-        })
+exports.deleteQuiz = catchAsync(async(req, res, next) => {
+    const quiz = await Quiz.findByIdAndDelete(req.params.id)
+    if (!quiz) {
+        return "No Quiz with that ID"
+    }
+    res.status(204).json({
+        status: "success",
+        data: null
     })
+})
 
-exports.updateQuiz = () =>
-    async(req, res) => {
-        const quiz = await Quiz.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!quiz) {
-            return "No Quiz with that ID"
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                data: quiz
-            }
-        })
-    }
-
-exports.createQuiz = () =>
-    async(req, res) => {
-        const quiz = await Quiz.create(req.body)
-        res.status(201).json({
-            status: "success",
-            data: {
-                data: quiz
-            }
-        })
-    }
-
-exports.getQuiz = () =>
-    async(req, res) => {
-        let query = await Quiz.findById(req.params.id)
-        const quiz = await query
-        if (!quiz) {
-            return "No Quiz with that ID"
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                data: quiz
-            }
-        })
-    }
-
-exports.getQuizzes = () =>
-    catchAsync(async(req, res, next) => {
-        let filter = {}
-        console.log("We are in the getQuizzes method")
-        if (req.params.topic) {
-            filter = { topic: req.params.topic }
-        }
-        console.log("We are going to get the query")
-        const query = Quiz.find(filter)
-        console.log("We will now fetch the quizzes")
-        const quizzes = await query
-        console.log(`We should have some quizzes ${quizzes}`)
-        res.status(200).json({
-            status: "success",
-            results: quizzes.length,
-            data: {
-                data: quizzes
-            }
-        })
+exports.updateQuiz = catchAsync(async(req, res, next) => {
+    const quiz = await Quiz.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
     })
+    if (!quiz) {
+        return "No Quiz with that ID"
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            data: quiz
+        }
+    })
+})
+
+exports.createQuiz = catchAsync(async(req, res) => {
+    const quiz = await Quiz.create(req.body)
+    res.status(201).json({
+        status: "success",
+        data: {
+            data: quiz
+        }
+    })
+})
+
+exports.getQuiz = catchAsync(async(req, res) => {
+    let query = await Quiz.findById(req.params.id)
+    const quiz = await query
+    if (!quiz) {
+        return "No Quiz with that ID"
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            data: quiz
+        }
+    })
+})
+
+exports.getQuizzes = catchAsync(async(req, res, next) => {
+    let filter = {}
+    if (req.params.topic) {
+        filter = { topic: req.params.topic }
+    }
+    const query = Quiz.find(filter)
+    const quizzes = await query
+    res.status(200).json({
+        status: "success",
+        results: quizzes.length,
+        data: {
+            data: quizzes
+        }
+    })
+})
