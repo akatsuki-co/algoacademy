38f6cb3b88e15e68728d87f620f4f48cda982747
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-06-15 16:05:32 -0700

    Add pem file and update endpoint

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const router = express.Router()
 // Mounting Routers - API endpoints
 app.use("/", router)
 app.use("/api/v1/quizzes", quizRouter)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 // all other routes
 app.get("*", (req, res) => {
