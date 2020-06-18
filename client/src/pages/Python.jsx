5e74a56466d595ad5fabe34be307ba486ca432dd
Tu Vo <tuvo@macbook-pro.attlocal.net>
2020-06-18 15:11:58 -0700

    Update fetch process

--- client/src/pages/Python.jsx
+++ client/src/pages/Python.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState, useEffect } from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -8,48 +8,52 @@ import { Switch, Route } from 'react-router-dom'
 import fetchTable from '../utils/fetchTable'
 
 const Python = () => {
-    const [table, setTable] = useState([])
+  const initialState = {
+    sidebar: [],
+    language: '',
+  }
+  const [table, setTable] = useState(initialState)
 
-    useEffect(() => {
-        const fetchT = async () => {
-            try {
-                const menu = await fetchTable("python")
-                setTable(menu.data[0])
-                console.log("In Python useEffect")
-                console.log(menu.data)
-            }
-            catch (err) {
-                console.log(err)
-            }
-        }
-        fetchT()
-    }, []);
+  useEffect(() => {
+    const fetchT = async () => {
+      try {
+        const tempTable = await fetchTable('python')
+        const { sidebar, language } = tempTable.data[0]
+        setTable(() => {
+          return { sidebar, language }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchT()
+  }, [])
 
-    return (
-        <section className='py-3'>
-            <Container>
-                <Row>
-                    <Sidebar data={table}></Sidebar>
-                    <Switch>
-                        <Route
-                            path='/python/:topic'
-                            render={(props) => <Content {...props} language='python' />}
-                        />
-                        <Route
-                            path='/'
-                            render={(props) => (
-                                <Content
-                                    {...props}
-                                    language="python"
-                                    defaultTopic="helloWorld"
-                                />
-                            )}
-                        />
-                    </Switch>
-                </Row>
-            </Container>
-        </section>
-    )
+  return (
+    <section className="py-3">
+      <Container>
+        <Row>
+          <Sidebar data={table}></Sidebar>
+          <Switch>
+            <Route
+              path="/python/:topic"
+              render={(props) => <Content {...props} language="python" />}
+            />
+            <Route
+              path="/"
+              render={(props) => (
+                <Content
+                  {...props}
+                  language="python"
+                  defaultTopic="helloWorld"
+                />
+              )}
+            />
+          </Switch>
+        </Row>
+      </Container>
+    </section>
+  )
 }
 
 export default Python
