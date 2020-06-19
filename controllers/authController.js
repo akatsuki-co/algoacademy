f8cbd108923c73ec6e4866a26b87cb8bffb55cdd
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-06-19 11:34:49 -0700

    Add protect to routes needing auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("./../models/User")
 const catchAsync = require("./../utils/catchAsync")
 const AppError = require('./../utils/appError')
 const jwt = require("jsonwebtoken")
+const { promisify  } = require('util')
 
 
 const signToken = id => {
@@ -62,3 +63,37 @@ exports.logout = (req, res) => {
         status: 'success'
     })
 }
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // get token and check if it exists
+    let token
+    const auth = req.headers.authorization
+    if (auth && auth.startsWith('Bearer')) {
+        token = auth.split(' ')[1]
+    } else if (req.cookies && req.cookies.jwt) {
+        token = req.cookies.jwt
+    } else {
+        return next(
+            new AppError('You are not logged in! Please log in to get access.', 401)
+        )}
+
+    if (!token) {
+        return next(
+            new AppError('You are not logged in! Please log in to get access.', 401)
+        )
+    }
+    // validate token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+    // check if user still exists
+    const freshUser = await User.findById(decoded.id)
+    if (!freshUser) {
+        return next(
+            new AppError('The user belonging to this token no longer exists.', 401)
+        )
+    }
+    // grant access to protected route
+    req.user = freshUser
+    res.locals.user = freshUser
+    next()
+})
