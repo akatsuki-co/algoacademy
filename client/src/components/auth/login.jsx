166b403f4e35ee4f02589feec297507cfa8a5827
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-06-19 12:20:16 -0700

    Remove Col import

--- client/src/components/auth/login.jsx
+++ client/src/components/auth/login.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 import './styles.css'
 
