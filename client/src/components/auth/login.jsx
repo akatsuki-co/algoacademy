feafd929a957f16f65b61d7b9c6bd6b7dc9725d7
Tu Vo <tuvo1106@gmail.com>
2020-06-24 17:41:48 -0700

    Fix styling in login button

--- client/src/components/auth/login.jsx
+++ client/src/components/auth/login.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 
 import './styles.css'
 
 const Login = () => {
   return (
-    <div className="container py-5">
-      <Row className="justify-content-md-center">
-        <Form className="login-form ">
-          <Form.Group controlId="formBasicEmail">
+    <div className='container py-5'>
+      <Row className='justify-content-md-center'>
+        <Form className='login-form '>
+          <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type='email' placeholder='Enter email' />
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type='password' placeholder='Password' />
           </Form.Group>
-          <Row className="justify-content-md-center py-2">
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+          <Row className='justify-content-md-center py-2'>
+            <button className='login-btn'>Submit</button>
           </Row>
         </Form>
       </Row>
