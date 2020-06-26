dd32a1ad47ef114514da998a56227ecb9b478a4f
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-06-25 21:29:09 -0700

    Add protect on contribute route

--- routes/contributeRoutes.js
+++ routes/contributeRoutes.js
@@ -2,11 +2,12 @@ const express = require("express")
 const {
     contribute,
 } = require("./../controllers/contributeController")
+const { protect  } = require('./../controllers/authController')
 
 const router = express.Router()
 
 router
     .route("/")
-    .post(contribute)
+    .post(protect, contribute)
 
 module.exports = router
