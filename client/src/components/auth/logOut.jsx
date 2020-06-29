77ff87187ab84cffa656424a0aacbeb24a4488e1
Tu Vo <tuvo1106@gmail.com>
2020-06-29 15:43:44 -0700

    Add logOut component

---
+++ client/src/components/auth/logOut.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import UserContext from '../../context/UserContext'
+import { useHistory } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+
+import './styles.css'
+
+const LogOut = () => {
+  const { setActiveUser } = useContext(UserContext)
+  const [, , removeCookie] = useCookies([])
+  const history = useHistory()
+  useEffect(() => {
+    removeCookie('jwt')
+    setActiveUser('')
+    setTimeout(() => history.push('/'), 3000)
+  })
+  return (
+    <>
+      <Row className='justify-content-md-center py-3'>
+        <h3>You have successfully logged out.</h3>
+      </Row>
+      <Row className='justify-content-md-center'>
+        <h4>You will be redirected shortly.</h4>
+      </Row>
+    </>
+  )
+}
+
+export default LogOut
