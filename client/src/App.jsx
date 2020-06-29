b59ccbc2239b514e3f174ccc9b45012faef8daf1
Tu Vo <tuvo1106@gmail.com>
2020-06-29 15:44:50 -0700

    Add activeUser state to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AppRouter from './components'
+import UserContext from './context/UserContext'
+import { useCookies } from 'react-cookie'
 
 import './App.css'
 
 const App = () => {
+  const [cookies] = useCookies(['jwt'])
+  const [activeUser, setActiveUser] = useState('')
+  useEffect(() => {
+    if (cookies && cookies.jwt) {
+      setActiveUser(cookies.jwt)
+    }
+  }, [cookies])
   return (
     <div className='App'>
-      <AppRouter />
+      <UserContext.Provider value={{ activeUser, setActiveUser }}>
+        <AppRouter />
+      </UserContext.Provider>
     </div>
   )
 }
