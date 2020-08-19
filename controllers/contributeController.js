f1cdab0b812e0f1da6fe5e6d8a34576c1868c8e9
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-08-19 13:43:08 -0700

    Refactor contributeController

--- controllers/contributeController.js
+++ controllers/contributeController.js
@@ -1,19 +1,22 @@
 const catchAsync = require("./../utils/catchAsync")
 const AppError = require('./../utils/appError')
+const slugify = require('./../utils/slugify')
 const fs = require('fs')
 
+const CONTRIBUTE_URL = './client/src/docs/contributions/'
+
 exports.contribute = catchAsync(async(req, res, next) => {
-    const username = req.body["username"].replace(/\s+/g, '_').toLowerCase()
-    const topic = req.body["topic"].replace(/\s+/g, '_').toLowerCase()
-    const markdown = req.body["markdown"]
-    const today = new Date()
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
-    fs.writeFile(`./client/src/docs/contributions/${date}_${username}-${topic}.md`, markdown, function (err) {
-        if (err) throw err;
-        console.log('File is created successfully.');
-    })
-    await res.status(200).json({
-        status: "success"
-    })
+  const username = slugify(req.body["username"])
+  const topic = slugify(req.body["topic"])
+  const markdown = req.body["markdown"]
+  const today = new Date()
+  const date = `${today.getFullYear()}-${(today.getMonth() + 1)}-${today.getDate()}`
+  fs.writeFile(`${CONTRIBUTE_URL}${date}_${username}-${topic}.md`, markdown, function (err) {
+    if (err) throw err;
+    console.log('File is created successfully.');
+  })
+  await res.status(200).json({
+    status: "success"
+  })
 })
 
